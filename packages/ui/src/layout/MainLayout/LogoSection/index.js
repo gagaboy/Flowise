c85392d88cf820d13cb08b7500c4f5d4c30e099c
Eric Chang <gagaboy@126.com>
2023-09-10 16:15:31 +0800

    修复点击logo的菜单高亮的bug

--- packages/ui/src/layout/MainLayout/LogoSection/index.js
+++ packages/ui/src/layout/MainLayout/LogoSection/index.js
@@ -7,12 +7,22 @@ import { ButtonBase } from '@mui/material'
 import config from 'config'
 import Logo from 'ui-component/extended/Logo'
 
+// project imports
+import { MENU_OPEN, SET_MENU } from 'store/actions'
+import { useDispatch } from 'react-redux'
+
 // ==============================|| MAIN LOGO ||============================== //
 
-const LogoSection = () => (
-    <ButtonBase disableRipple component={Link} to={config.defaultPath}>
-        <Logo />
-    </ButtonBase>
-)
+const LogoSection = () => {
+    const dispatch = useDispatch()
+    const logoClick = () => {
+        dispatch({ type: MENU_OPEN, id: 'chatflows' })
+    }
+    return (
+        <ButtonBase disableRipple component={Link} to={config.defaultPath} onClick={logoClick}>
+            <Logo />
+        </ButtonBase>
+    )
+}
 
 export default LogoSection
